26e84003b63140c34cf998b56bdfc8f060edce9f
egor_chetverikov <egor.chetverikov.travelfactory.fr>
2025-04-14 17:56:59 +0300

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/JuliaGraphDesign/', // This is your repository name
   plugins: [
     vue(),
     vueDevTools(),
